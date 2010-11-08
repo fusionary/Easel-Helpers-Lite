661b2f6278fcc545e21744cf9c4d9f305e68f342
Jason Stewart <jstewart@fusionary.com>
2010-11-08 16:44:30 -0500

    Tweaks to helper and gemspec

--- lib/easel_helpers_lite/helpers.rb
+++ lib/easel_helpers_lite/helpers.rb
@@ -21,5 +21,19 @@ module EaselHelpersLite
       (doc/:a).add_class("btn")
       raw(doc.to_html)
     end
+
+    def submit_button(*args, &block)
+      button_text = args.first
+      options = args.extract_options!
+      klass = options[:class] || "btn"
+
+      content_tag("button", :type => "submit", :class => klass) do
+        if block_given?
+          yield
+        else
+          content_tag("span", button_text)
+        end
+      end
+    end
   end
 end
